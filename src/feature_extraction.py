2e051ad229d831e6083af15a49ac062c96b50585
vibhor9011 <vibhor.25mim10093@vitbhopal.ac.in>
2026-09-17 14:45:19 +0530

    spaces removed

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,3 +1,5 @@
 FEATURES=["study_hours","attendance","previous_marks","assignment_score","sleep_hours"]
-def create_feature_row(study_hours,attendance,previous_marks,assignment_score,sleep_hours): return [[float(study_hours),float(attendance),float(previous_marks),float(assignment_score),float(sleep_hours)]]
-def get_feature_names(): return FEATURES.copy()
+def create_feature_row(study_hours,attendance,previous_marks,assignment_score,sleep_hours): 
+    return [[float(study_hours),float(attendance),float(previous_marks),float(assignment_score),float(sleep_hours)]]
+def get_feature_names(): 
+    return FEATURES.copy()
